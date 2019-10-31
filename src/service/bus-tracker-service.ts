e5894fa67256e675de84c973485ea067f0ae0ca8
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-10-31 11:17:45 -0500

    feat: data caching

--- src/service/bus-tracker-service.ts
+++ src/service/bus-tracker-service.ts
@@ -5,41 +5,63 @@ import {BusDirection} from "../models/bus-direction";
 
 export class BusTrackerService {
     private readonly client: BusTrackerClient;
-    private readonly directionMap: Map<string, BusDirection[]>;
+    private routeListCache: BusRoute[];
+    private readonly directionMapCache: Map<string, BusDirection[]>;
+    private readonly stopMapCache: Map<string, Map<string, BusStop[]>>;
 
     constructor() {
         this.client = new BusTrackerClient();
-        this.directionMap = new Map<string, BusDirection[]>();
+        this.routeListCache = [];
+        this.directionMapCache = new Map<string, BusDirection[]>();
+        this.stopMapCache = new Map<string, Map<string, BusStop[]>>();
     }
 
     async getRoutes(): Promise<BusRoute[]> {
-        return this.client.getRoutes();
+        let cachedRoutes = this.routeListCache;
+        if(cachedRoutes.length > 0) {
+            return cachedRoutes;
+        }
+        
+        return this.client.getRoutes()
+            .then((routes: BusRoute[]) => {
+                this.routeListCache = routes;
+                return routes;
+            })
     }
 
     async getDirections(routeId: string): Promise<BusDirection[]> {
-        let directions: BusDirection[] | undefined = this.directionMap.get(routeId);
-        if (!directions || directions.length === 0) {
-            directions = await this.client.getDirections(routeId);
-            directions.forEach((direction: BusDirection) => {
-                direction.routeId = routeId;
-            });
-            this.directionMap.set(routeId, directions);
+        let cachedDirections: BusDirection[] | undefined = this.getCachedData(routeId, this.directionMapCache);
+        if (cachedDirections && cachedDirections.length > 0) {
+            return cachedDirections;
         }
-        return directions;
+
+        return this.client.getDirections(routeId)
+            .then((directions: BusDirection[]) => {
+                directions.forEach((direction: BusDirection) => {
+                    direction.routeId = routeId;
+                });
+                this.cacheData(routeId, directions, this.directionMapCache);
+                return directions;
+            });
     }
 
     async getStops(routeId: string, direction: string): Promise<BusStop[]> {
-        let oppositeDirectionStopMap: Map<string, string> = new Map<string, string>();
+        let cachedStopMap: Map<string, BusStop[]> = this.getCachedStopMap(routeId);
+        let cachedStops: BusStop[] | undefined = this.getCachedData(direction, cachedStopMap);
+        if (cachedStops && cachedStops.length > 0) {
+            return cachedStops;
+        }
+
+        let oppositeDirectionStopIdMap: Map<string, string> = new Map<string, string>();
         let availableDirections: BusDirection[] = await this.getDirections(routeId);
         let oppositeDirection: BusDirection | undefined = availableDirections
             .filter((dir: BusDirection) => dir.direction !== direction)
             .shift();
-
         if (oppositeDirection) {
             await this.client.getStops(routeId, oppositeDirection.direction)
                 .then((stops: BusStop[]) => {
                     stops.forEach((stop: BusStop) => {
-                        oppositeDirectionStopMap.set(stop.name, stop.id);
+                        oppositeDirectionStopIdMap.set(stop.name, stop.id);
                     });
                 });
         }
@@ -49,12 +71,27 @@ export class BusTrackerService {
                 stops.forEach((stop: BusStop) => {
                     stop.routeId = routeId;
                     stop.direction = direction;
-                    let oppositeDirectionId = oppositeDirectionStopMap.get(stop.name);
-                    if (oppositeDirectionId) {
-                        stop.oppositeDirectionStopId = oppositeDirectionId;
+                    let oppositeDirectionStopId = oppositeDirectionStopIdMap.get(stop.name);
+                    if (oppositeDirectionStopId) {
+                        stop.oppositeDirectionStopId = oppositeDirectionStopId;
                     }
                 });
+                this.cacheData(direction, stops, cachedStopMap);
+                this.cacheData(routeId, cachedStopMap, this.stopMapCache);
                 return stops;
             });
+    }
+
+    private getCachedStopMap(routeId: string): Map<string, BusStop[]> {
+        let cachedStopMap: Map<string, BusStop[]> | undefined = this.getCachedData(routeId, this.stopMapCache);
+        return cachedStopMap ? cachedStopMap : new Map<string, BusStop[]>();
+    }
+
+    private getCachedData<T>(key: string, cacheMap: Map<string, T>): T | undefined {
+        return cacheMap.get(key);
+    }
+
+    private cacheData<T>(key: string, value: T, cacheMap: Map<string, T>): void {
+        cacheMap.set(key, value);
     }
 }
