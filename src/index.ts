2662155b93ca4efabbd8a98bd8a88c0436c5e5e6
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-02 15:49:23 -0500

    feat: swagger

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,21 @@ import {BusRoute} from "./models/bus-route";
 import {BusStop} from "./models/bus-stop";
 import {BusDirection} from "./models/bus-direction";
 import {BusPrediction} from "./models/bus-prediction";
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './resources/swagger.json'
+import * as bodyParser from 'body-parser'
 
 const app = express();
 const port = 8080;
+const service = new BusTrackerService();
 
-let service = new BusTrackerService();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+    res.redirect('/swagger');
+});
 
 app.get("/routes", (req, res) => {
     service.getRoutes()
